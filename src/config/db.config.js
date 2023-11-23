a7ec326eecf15292b450e0f9d314aeb58aeb1a4a
acp2 <arielcontrerasp@gmail.com>
2023-11-23 03:19:47 -0300

    listo para heroku

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,13 +1,15 @@
+require('dotenv').config()
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'aconpardo_condominio',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 100,
     queueLimit: 0,
+
 });
 
 //local
